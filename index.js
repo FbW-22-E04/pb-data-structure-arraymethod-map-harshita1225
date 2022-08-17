00ac380937ccf8a353e09bdbf03cbb78a6f0333a
harshita <harshita1225@gmail.com>
2022-08-17 18:59:54 +0200

    task completed

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 // Write the function camelize(str) that changes dash-separated words like “my-short-string” into camel-cased “myShortString”.
 //1
 function camelize(str) {
- //write your code here
+  const words = str.split("-");
+  //console.log(words);
+  const camelCase = words.map((letters, indx) => {
+    if (indx > 0) {
+      return letters[0].toUpperCase() + letters.slice(1);
+    }
+    return letters;
+  });
+
+  return camelCase.join("");
 }
 
-console.log(camelize("background-color"))
-console.log(camelize("list-style-image"))
-console.log(camelize("-webkit-transition"))
+console.log(camelize("background-color"));
+console.log(camelize("list-style-image"));
+console.log(camelize("-webkit-transition"));
 
 //Write the code to create another array from it, of objects with id and fullName, where fullName is generated from name and surname.
 //2
@@ -17,13 +26,13 @@ let mary = { name: "Mary", surname: "Key", id: 3 };
 
 let users = [john, pete, mary];
 
-//write your code here
+const usersMapped = users.map(function (user) {
+  const newUser = {};
+  newUser.fullName = `${user.name} ${user.surname}`;
+  newUser.id = `${user.id}`;
 
-/*   usersMapped = [
-    { fullName: "John Smith", id: 1 },
-    { fullName: "Pete Hunt", id: 2 },
-    { fullName: "Mary Key", id: 3 }
-  ] */
+  return newUser;
+});
 
 console.log(usersMapped[0].id); // 1
 console.log(usersMapped[0].fullName); // John Smith
